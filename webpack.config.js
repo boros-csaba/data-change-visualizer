94b5dff7cfe9e59f0ab802e28363e94b792498ae
Csaba Boros <boros.csaba94@gmail.com>
2024-01-07 13:16:21 +0100

    Setup webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, '/src/index.js'),
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true
+    },
 }
