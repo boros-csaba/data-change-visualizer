d2a0c9942a7bcdf2b6b57dca5f121e430bbcb23a
Csaba Boros <boros.csaba94@gmail.com>
2024-01-07 14:27:42 +0100

    Video recording

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 200, 200 );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -14,6 +14,12 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+var canvas = document.querySelector('canvas');
+var video = document.querySelector('video');
+var videoStream = canvas.captureStream();
+var mediaRecorder = new MediaRecorder(videoStream);
+mediaRecorder.start();
+
 function animate() {
 	requestAnimationFrame( animate );
 
@@ -21,6 +27,28 @@ function animate() {
 	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
+    videoStream.getVideoTracks()[0].requestFrame();
 }
 
 animate();
+
+
+
+var chunks = [];
+mediaRecorder.ondataavailable = function(e) {
+  chunks.push(e.data);
+  console.log("Data available")
+};
+
+mediaRecorder.onstop = function(e) {
+  var blob = new Blob(chunks, { 'type' : 'video/mp4' });
+  chunks = [];
+    var videoURL = URL.createObjectURL(blob);
+    video.src = videoURL;
+  };
+  
+
+setTimeout(function () { 
+  mediaRecorder.stop(); 
+  console.log("Stop")
+}, 5000);
