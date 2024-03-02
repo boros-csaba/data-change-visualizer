898bf188a786c509edc5d969903e971ddf4d5779
Csaba Boros <boros.csaba94@gmail.com>
2024-03-03 00:27:28 +0100

    Tabs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,14 @@ import '../styles/styles.scss'
 import { startAnimation } from './animation.js';
 
 startAnimation();
+
+document.getElementById('tab1').addEventListener('click', (_) => setActiveTab(1));
+document.getElementById('tab2').addEventListener('click', (_) => setActiveTab(2));
+document.getElementById('tab3').addEventListener('click', (_) => setActiveTab(3));
+
+function setActiveTab(tab) {
+    document.getElementById('tab1').classList.remove('active');
+    document.getElementById('tab2').classList.remove('active');
+    document.getElementById('tab3').classList.remove('active');
+    document.getElementById(`tab${tab}`).classList.add('active');
+}
