9a822470e2c83de9a924fa31b31ab098d17f69de
Csaba Boros <boros.csaba94@gmail.com>
2024-04-26 16:55:01 +0200

    Create lambda using cdk

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunction = new lambda.Function(this, 'CreateStripeCheckoutSession', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda_functions/create_stripe_checkout_session/'),
+      environment: {
+        STRIPE_SECRET_KEY: ''
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE
+    });
   }
 }
