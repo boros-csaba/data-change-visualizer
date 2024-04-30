52cc9639ce1f5d1db1de8d176f4e074d3bfeeacf
Csaba Boros <boros.csaba94@gmail.com>
2024-04-30 13:15:15 +0200

    Fixes

--- infrastructure/lib/website.ts
+++ infrastructure/lib/website.ts
@@ -13,7 +13,11 @@ export class Website extends Construct {
         versioned: false,
         publicReadAccess: false,
         blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-        removalPolicy: cdk.RemovalPolicy.DESTROY
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        enforceSSL: true,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        accessControl: s3.BucketAccessControl.PRIVATE,
+        objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_PREFERRED
       });
 
       new cf.Distribution(this, 'ViralChartVideosWebsiteDistribution', {
@@ -21,7 +25,10 @@ export class Website extends Construct {
           origin: new cfo.S3Origin(bucket),
           compress: true,
           allowedMethods: cf.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-          viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+          viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          originRequestPolicy: cf.OriginRequestPolicy.CORS_S3_ORIGIN,
+          cachedMethods: cf.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+          responseHeadersPolicy: cf.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS
         },
         defaultRootObject: 'index.html'
       });
