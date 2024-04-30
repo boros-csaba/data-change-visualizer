7c158d672eb4943f6162280181a6e23ec18bd3a9
Csaba Boros <boros.csaba94@gmail.com>
2024-04-30 15:11:53 +0200

    Fix

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -6,7 +6,8 @@ import { InfrastructureStack } from '../lib/infrastructure-stack';
 const app = new cdk.App();
 new InfrastructureStack(app, 'ViralChartVideos', {
   
-  env: {  
+  env: { 
+    account: process.env.AWS_ACCOUNT_ID,
     region: process.env.AWS_REGION 
   }
 
