1a9f1d1fed910c750e3680d7446cf882679e075c
Csaba Boros <boros.csaba94@gmail.com>
2024-05-03 20:59:54 +0200

    Refactor

---
+++ src/scripts/file-upload-handler.js
@@ -0,0 +1,89 @@
+import { read, utils } from 'xlsx';
+import { setupWithNewData } from './data.js';
+
+export class FileUploadHandler {
+    initFileUploadInput(animation) {
+        this.addFileUploadEventListeners(animation);
+
+        const fileInput = document.querySelector('input[type="file"]');
+        const dropArea = document.querySelector('.file-drop-area');
+
+        fileInput.addEventListener('dragenter', function () {
+            dropArea.classList.add('is-active');
+        });
+
+        fileInput.addEventListener('focus', function () {
+            dropArea.classList.add('is-active');
+        });
+
+        fileInput.addEventListener('click', function () {
+            dropArea.classList.add('is-active');
+        });
+
+        fileInput.addEventListener('dragleave blur drop', function () {
+            dropArea.classList.remove('is-active');
+        });
+
+        fileInput.addEventListener('blur', function () {
+            dropArea.classList.remove('is-active');
+        });
+
+        fileInput.addEventListener('drop', function () {
+            dropArea.classList.remove('is-active');
+        });
+
+        fileInput.addEventListener('change', function () {
+            /*var filesCount = $(this)[0].files.length;
+    var $textContainer = $(this).prev();
+  
+    if (filesCount === 1) {
+      var fileName = $(this).val().split('\\').pop();
+      $textContainer.text(fileName);
+    } else {
+      $textContainer.text(filesCount + ' files selected');
+    }*/
+        });
+    }
+
+    addFileUploadEventListeners(animation) {
+        const fileInput = document.querySelector('input[type="file"]');
+        fileInput.addEventListener('change', () => this.handleFileUpload(animation));
+    }
+
+    handleFileUpload(animation) {
+        console.log('File uploaded', animation);
+        const fileInput = document.querySelector('input[type="file"]');
+        const file = fileInput.files[0];
+        console.log(file);
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(file);
+        fileReader.onload = (e) => this.onFileLoad(e, animation);
+    }
+
+    onFileLoad(event, animation) {
+        let fileData = event.target.result;
+        let workbook = read(
+            fileData,
+            { type: 'binary' },
+            { dateNF: 'mm/dd/yyyy' }
+        );
+        let sheet = workbook.Sheets[workbook.SheetNames[0]];
+        var rawData = utils.sheet_to_json(sheet, { header: 1 });
+        this.processData(rawData, animation);
+    }
+
+    processData(rawData, animation) {
+        let timeLabels = rawData.map((row) => row[0]).slice(1);
+        let items = rawData[0].slice(1).map((name, index) => {
+            return {
+                name: name,
+                data: rawData.slice(1).map((row) => row[index + 1]),
+            };
+        });
+
+        animation.stopAnimation();
+        animation.clearScene();
+        setupWithNewData(timeLabels, items);
+        animation.startAnimation();
+    }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,44 @@
-import "../styles/styles.scss";
-import { Animation } from "./animation.js";
-import { initFileUploadInput } from "./file-upload.js";
-import { download } from "./video.js";
+import '../styles/styles.scss';
+import { Animation } from './animation.js';
+import { FileUploadHandler } from './file-upload-handler.js';
+import { download } from './video.js';
+
+const myApp = {
+    uploadedFileData: null
+};
 
 const animation = new Animation();
-initFileUploadInput(animation);
+const fileUploadHandler = new FileUploadHandler();
+fileUploadHandler.initFileUploadInput(animation);
 animation.startAnimation();
 
 window.onload = () => {
-  document
-    .getElementsByClassName("download-button")[0]
-    .addEventListener("click", async () => {
-      let uploadUrl = await getS3PresignedUrl();
-      download();
-    });
+    document
+        .getElementsByClassName('download-button')[0]
+        .addEventListener('click', async () => {
+            let uploadUrl = await getS3PresignedUrl();
+            await uploadDataToS3(uploadUrl);
+            download();
+        });
 };
 
 async function getS3PresignedUrl() {
-  const url = new URL(
-    "https://5cf4ly6ngsvgtzjwh3xmjye6ki0hjkah.lambda-url.us-west-1.on.aws/"
-  );
-  let response = await fetch(url, {
-    method: "POST",
-  });
+    const url = new URL(
+        'https://5cf4ly6ngsvgtzjwh3xmjye6ki0hjkah.lambda-url.us-west-1.on.aws/'
+    );
+    let response = await fetch(url, {
+        method: 'POST',
+    });
 
-  return response.text();
+    return response.text();
+}
+
+async function uploadDataToS3(s3Url) {
+    const url = new URL(s3Url);
+    let response = await fetch(url, {
+        method: 'PUT',
+        body: animation.getVideoBlob(),
+    });
+
+    return response.text();
 }
