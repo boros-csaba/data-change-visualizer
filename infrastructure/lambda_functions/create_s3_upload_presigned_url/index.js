4a30530d52e355ff4c6fb56e8363c44ba8b4e3b7
Csaba Boros <boros.csaba94@gmail.com>
2024-05-03 14:20:15 +0200

    Fix

--- infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
+++ infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
@@ -1,39 +1,13 @@
 const { randomUUID } = require('crypto');
-const AWS = require('aws-sdk');
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({ region: process.env.BUCKET_NAME });
 
 exports.handler = async function (_) {
 
-    const objectKey = randomUUID();
-
-    const s3Policy = {
-        Version: '2012-10-17',
-        Statement: [{
-            Effect: 'Allow',
-            Action: 's3:GetObject',
-            Resource: process.env.BUCKET_ARN + '/' + objectKey
-        }]
-    };
-
-    var credentials = await new AWS.ChainableTemporaryCredentials({
-        params: {
-            RoleArn: process.env.PRESIGN_URL_ROLE_ARN,
-            Policy: JSON.stringify(s3Policy),
-            DurationSeconds: expireSeconds
-        },
-        Credentials: {
-            AccessKeyId: AWS.config.credentials.AccessKeyId,
-            SecretAccessKey: AWS.config.credentials.SecretAccessKey,
-            SessionToken: AWS.config.credentials.SessionToken
-        }
-    });
-
-    const s3 = new AWS.S3(new AWS.Config({
-        credentials: credentials
-    }));
-
-    return await s3.getSignedUrl('putObject', {
-        Bucket: process.env.BUCKET_NAME,
-        Key: objectKey
-    });
+    const key = randomUUID();
+    const command = new PutObjectCommand({ Bucket: process.env.BUCKET_NAME, Key: key });
+    return getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
 }
--- infrastructure/lib/video-source-files.ts
+++ infrastructure/lib/video-source-files.ts
@@ -38,7 +38,8 @@ export class VideoSourceFiles extends Construct {
         environment: {
           BUCKET_NAME: s3Bucket.bucketName,
           BUCKET_ARN: s3Bucket.bucketArn,
-          PRESIGN_URL_ROLE_ARN: presignUrlRole.roleArn
+          PRESIGN_URL_ROLE_ARN: presignUrlRole.roleArn,
+          AWS_REGION: process.env.AWS_REGION || ''
         }
       });
   }
