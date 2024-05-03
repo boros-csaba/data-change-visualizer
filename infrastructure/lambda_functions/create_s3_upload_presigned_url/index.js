6eb862d33c7ae1398b07e3cec2e8289b4d01caa6
Csaba Boros <boros.csaba94@gmail.com>
2024-05-03 14:25:41 +0200

    Fix

--- infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
+++ infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
@@ -4,7 +4,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({ region: process.env.BUCKET_NAME });
 
-exports.handler = async function (_) {
+export const handler = async (_) => {
 
     const key = randomUUID();
     const command = new PutObjectCommand({ Bucket: process.env.BUCKET_NAME, Key: key });
