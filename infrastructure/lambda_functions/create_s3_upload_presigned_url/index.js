64b2abe252d6c90581c4a1b01acfd96cd19bccff
Csaba Boros <boros.csaba94@gmail.com>
2024-05-04 18:13:00 +0200

    Fix

--- infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
+++ infrastructure/lambda_functions/create_s3_upload_presigned_url/index.js
@@ -2,17 +2,12 @@ const { randomUUID } = require('crypto');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
-const s3Client = new S3Client({ region: process.env.AWS_REGION });
+const s3Client = new S3Client({ region: process.env.BUCKET_NAME });
 
 exports.handler = async function (_) {
 
     const key = randomUUID();
     const command = new PutObjectCommand({ Bucket: process.env.BUCKET_NAME, Key: key });
-    const url = getSignedUrl(s3Client, command, { expiresIn: 3600 });
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(url)
-    }
+    return getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
 }
