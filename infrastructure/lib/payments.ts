b2b9d62d885ba1d3f5137bf00946e25b259f226b
Csaba Boros <boros.csaba94@gmail.com>
2024-05-05 11:50:19 +0200

    Use nodejs function

--- infrastructure/lib/payments.ts
+++ infrastructure/lib/payments.ts
@@ -1,19 +1,18 @@
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class Payments extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        const createStripeCheckoutSessionLambdaFunction = new lambda.Function(
+        const createStripeCheckoutSessionLambdaFunction = new nodejs.NodejsFunction(
             this,
             'CreateStripeCheckoutSession',
             {
                 runtime: lambda.Runtime.NODEJS_LATEST,
                 handler: 'index.handler',
-                code: lambda.Code.fromAsset(
-                    'lambda_functions/create_stripe_checkout_session/'
-                ),
+                entry: 'lambda_functions/create_stripe_checkout_session/index.js',
                 environment: {
                     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || '',
                     STRIPE_PRICE_ID: process.env.STRIPE_PRICE_ID || '',
