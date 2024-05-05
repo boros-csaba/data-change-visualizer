36355bda0d91e8399a2a006d17cd9ae48fcd3662
Csaba Boros <boros.csaba94@gmail.com>
2024-05-05 12:14:09 +0200

    Fix4

--- infrastructure/lib/payments.ts
+++ infrastructure/lib/payments.ts
@@ -1,15 +1,19 @@
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import path = require('path');
 
 export class Payments extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
+        const lambdaDir = path.resolve(__dirname, '../lambda_functions/create_stripe_checkout_session');
+
         const createStripeCheckoutSessionLambdaFunction = new nodejs.NodejsFunction(
             this,
             'CreateStripeCheckoutSession',
             {
+                projectRoot: lambdaDir,
                 runtime: lambda.Runtime.NODEJS_LATEST,
                 handler: 'handler',
                 entry: '../lambda_functions/create_stripe_checkout_session/index.js',
