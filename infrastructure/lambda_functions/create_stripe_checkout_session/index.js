877c4c0558826db1bb7d8584e3478144d731e1a5
Csaba Boros <boros.csaba94@gmail.com>
2024-05-07 11:06:07 +0200

    Fix stripe lambda

--- infrastructure/lambda_functions/create_stripe_checkout_session/index.js
+++ infrastructure/lambda_functions/create_stripe_checkout_session/index.js
@@ -3,16 +3,12 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async function (event) {
 
-    console.log('event', event);
-    console.log('slen', process.env.STRIPE_SECRET_KEY.length);
-    console.log('priceid', process.env.STRIPE_PRICE_ID);
-
     const session = await stripe.checkout.sessions.create({
         line_items: [{
             price: process.env.STRIPE_PRICE_ID,
             quantity: 1
         }],
-        clientReferenceId: event.body,
+        client_reference_id: event.body,
         mode: 'payment',
         success_url: 'https://localhost/success',
         cancel_url: 'https://localhost/cancel',
